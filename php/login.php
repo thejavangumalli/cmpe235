658638587aef192a2eb68979c05c864fd2801aa4
Theja Vangumalli <theja.vangumalli@gmail.com>
2014-12-03 01:54:25 -0800

    Complete student code and UI changes

--- php/login.php
+++ php/login.php
@@ -11,21 +11,32 @@ $query="SELECT id,firstname,lastname,phone,email,type from user where id =".$use
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
+  $gradesQuery = "SELECT homeworks,labs,midterm,presentation,project,final from student_grade where student_id=".$username;
+  $gradesResult = $conn->query($gradesQuery);
     while($row = $result->fetch_assoc()) {
-      $array = array(
-                              "firstname"=>$row["firstname"],
-                               "lastname"=>$row["lastname"],
-                               "email"=>$row["email"],
-                               "phone"=>$row["phone"],
-                               "type"=>$row["type"]
-                              );
+      while($gradesRow = $gradesResult->fetch_assoc())
+      {
+        $array = array(
+                      "firstname"=>$row["firstname"],
+                       "lastname"=>$row["lastname"],
+                       "email"=>$row["email"],
+                       "phone"=>$row["phone"],
+                       "type"=>$row["type"],
+                       "homeworks"=>$gradesRow["homeworks"],
+                       "labs"=>$gradesRow["labs"],
+                       "midterm"=>$gradesRow["midterm"],
+                       "presentation"=>$gradesRow["presentation"],
+                       "project"=>$gradesRow["project"],
+                       "final"=>$gradesRow["final"]
+                      );
+      }
     }
 }
 else
 {
      $array = array(
-                            "error"=>"Invalid Username/Password combination"
-                            );
+                    "error"=>"Invalid Username/Password combination"
+                    );
 }
 header('content-type: application/json; charset=utf-8');
 echo $_GET['callback'] . '(' . json_encode($array ). ')';
--- www/js/app.js
+++ www/js/app.js
@@ -15,13 +15,26 @@
               else
               {
                 //update the UI with student details
-                $('#firstname').text(result.firstname);
-                $('#lastname').text(result.lastname);
-                $('#phone').text(result.phone);
-                $('#email').text(result.email);
-                $('#id-info').text($username);
+                $('#firstname').val(result.firstname);
+                $('#lastname').val(result.lastname);
+                $('#phone').val(result.phone);
+                $('#email').val(result.email);
+                $('#id-info').val($username);
+                $('#homeworks').val(result.homeworks);
+                $('#labs').val(result.labs);
+                $('#midterm').val(result.midterm);
+                $('#presentation').val(result.presentation);
+                $('#project').val(result.project);
+                $('#final').val(result.final);
                 var type = result.type; //1 = student; 2= professor
-                window.location=window.location.href+"#mainPage"
+                if(type == 1)
+                  {
+                    window.location=window.location.href+"#studentMainPage"
+                  }
+                else
+                  {
+                    window.location=window.location.href+"#mainPage"
+                  }
               }
 
         });
